1b9ede68b3bc388362a9a2706dde4f96ba72aab4
Page <spage@fusionalliance.com>
2018-02-05 11:26:00 -0500

    Adding React-Redux bridging

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
+import {Provider} from 'react-redux';
+
+import configureStore from './redux/store/configureStore';
 
 import './App.css';
 
@@ -9,17 +12,22 @@ import About from './react/about/About';
 import Contact from './react/contact/Contact';
 
 function App(props) {
+  const store = configureStore();
+  // initialize store
+
   return (
-    <BrowserRouter>
-      <div>
-        <Header/>
-        <main>
-          <Route exact path="/" component={Home}/>
-          <Route path="/about" component={About}/>
-          <Route path="/contact" component={Contact}/>
-        </main>
-      </div>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <div>
+          <Header/>
+          <main>
+            <Route exact path="/" component={Home}/>
+            <Route path="/about" component={About}/>
+            <Route path="/contact" component={Contact}/>
+          </main>
+        </div>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
