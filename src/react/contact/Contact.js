b3b6711949e86a2458c49f6ce19610c8b4f8ee42
Page <spage@fusionalliance.com>
2018-02-06 10:46:25 -0500

    Working Redux

--- src/react/contact/Contact.js
+++ src/react/contact/Contact.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
 
-import ContactList from './ContactList';
+import ContactListContainer from './ContactListContainer';
 import ContactMaintenance from './ContactMaintenance';
 
 function Contact(props) {
@@ -10,7 +10,7 @@ function Contact(props) {
             <div>
                 <h2>Your Contacts</h2>
                 <div>
-                    <Route path="/contact/list" component={ContactList}/>
+                    <Route path="/contact/list" component={ContactListContainer}/>
                     <Route path="/contact/id/:id" component={ContactMaintenance}/>
                 </div>
             </div>
---
+++ src/react/contact/ContactListContainer.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+
+import * as contactActionCreators from '../../redux/actions/contactActionCreators';
+import ContactList from './ContactList';
+
+class ContactListContainer extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+    }
+
+    render() {
+        return (<ContactList contactList={this.props.contactList}/>);
+    }
+}
+
+ContactListContainer.propTypes = {
+    // injected by connect() below
+    contactList: PropTypes.array.isRequired,
+    actions: PropTypes.object.isRequired
+};
+
+const decoratedContactListContainer = connect(mapStateToProps, wrapActionCreatorsToProps)(ContactListContainer);
+
+function mapStateToProps(state, ownProps) {
+    return {"contactList": state.contactsState.contactList};
+}
+
+function wrapActionCreatorsToProps(dispatch) {
+    return {
+        // refactor
+        actions: bindActionCreators(contactActionCreators, dispatch)
+    };
+}
+
+export default decoratedContactListContainer;
